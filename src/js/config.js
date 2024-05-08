e553e28b8ff4299294fa6e7d07bb59e712576bad
Rodrigo <roinobre@gmail.com>
2024-05-07 23:04:40 -0400

    Initial Files

---
+++ src/js/config.js
@@ -0,0 +1,6 @@
+export const API_URL = `https://forkify-api.herokuapp.com/api/v2/recipes/`
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '3ad93435-2ed4-40c3-a379-ac75415bd60b'
+export const MODAL_CLOSE_SEC = 2.5;
+
